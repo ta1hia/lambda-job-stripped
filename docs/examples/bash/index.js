2fc5e72cddce01577eac5203073f2da382ca1a15
Tahia Khan <tahia.khan@gmail.com>
2015-02-06 15:21:28 -0500

    updating context so that bash example works

--- docs/examples/bash/index.js
+++ docs/examples/bash/index.js
@@ -5,9 +5,9 @@ var LambdaJob = require('lambda-job');
 var lambdaJob = new LambdaJob.LambdaJobWorker(AWS, jobReceived);
 exports.handler = lambdaJob.lambdaHandler;
 
-function jobReceived(params, errDataCallback) {
-  console.log("Will run: bash -c " + params.cmd + "...");
-  lambdaJob.execHelper(params.cmd, function(err, consoleOutput) {
-    errDataCallback(err, consoleOutput);
-  });
+function jobReceived(params, context,  errDataCallback) {
+	console.log("Will run: bash -c " + params.cmd + "...");
+	lambdaJob.execHelper(params.cmd, function(err, consoleOutput) {
+		errDataCallback(err, context, consoleOutput);
+	});
 }
